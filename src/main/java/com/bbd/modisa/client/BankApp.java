e84df1b77c7c195d4a79a9cffe66159bd2a03733
Modisaotsile Mokgethi <modisamokgethi@gmail.com>
2019-03-04 17:42:25 +0200

    Restructuring of methods

--- src/main/java/com/bbd/modisa/client/BankApp.java
+++ src/main/java/com/bbd/modisa/client/BankApp.java
@@ -10,12 +10,81 @@ import com.bbd.modisa.service.SavingsAccountService;
 import java.util.Scanner;
 
 public class BankApp {
+    private static AccountService accountService;
+    private static char cont = 'Y';
+    private static Scanner scanner = new Scanner(System.in);
+    private static double amount;
+    private static char accType;
+    private static AccountType acctType;
+
+    public static void createCheck()
+    {
+        accountService = new ChequeAccountService();
+        Account newAccC = accountService.createAccount(1);
+        System.out.println(newAccC.getAccountType() + " Account Created Successfully" + newAccC + "\n");
+
+        while (cont == 'Y') {
+
+
+            System.out.print("Which Action would you like to perform in your " + newAccC.getAccountType() +
+                    " Account? Deposit(D)/Withdrawal(W)/Check Balance(C): ");
+            char opt = scanner.next().charAt(0);
+
+            if (opt == 'D') {
+                System.out.print("Enter the amout you would like to Deposit: R ");
+                Scanner depAmount = new Scanner(System.in);
+                amount = depAmount.nextDouble();
+                System.out.print("Your Current Balance = R " + accountService.deposit(amount));
+
+            } else if (opt == 'W') {
+                System.out.print("Enter the amount to Withdraw: R");
+                Scanner withAmt = new Scanner(System.in);
+                amount = withAmt.nextDouble();
+                System.out.print("Withdraw Successfully\nYour Current Balance is R " + accountService.withdraw(amount));
+
+            } else if (opt == 'C') {
+                System.out.print("Your Current Balance is = R " + accountService.getBalance());
+            }
+            System.out.print("\nWould you like to perform another accountService?(Y/N): ");
+            cont = scanner.next().charAt(0);
+        }
+    }
+
+    public static void createSavings()
+    {
+        accountService = new SavingsAccountService();
+        Account saving = accountService.createAccount(1);
+        System.out.println(saving.getAccountType() + " Account Created Successfully" + saving+ "\n");
+
+        while (cont == 'Y') {
+            System.out.print("Which Action would you like to perform in your " + saving.getAccountType() +
+                    " Account? Deposit(D)/Withdrawal(W)/Check Balance(C): ");
+            char optS = scanner.next().charAt(0);
+            if (optS == 'D') {
+                Scanner depAmount = new Scanner(System.in);
+
+                System.out.print("Enter the amout you would like to Deposit: R");
+                amount = depAmount.nextDouble();
+
+                System.out.print("Your Current Balance = R " + accountService.deposit(amount));
+            } else if (optS == 'W') {
+                Scanner withAmt = new Scanner(System.in);
+
+                System.out.print("Enter the amount to Withdraw: R");
+                amount = withAmt.nextDouble();
+
+                accountService.withdraw(amount);
+                System.out.print("Withdraw Successfully\nYour Current Balance is R " + accountService.getBalance());
+            } else if (optS == 'C') {
+                System.out.print("Your Current Balance is = R " + accountService.getBalance());
+            }
+            System.out.print("\nWould you like to perform another accountService?(Y/N): ");
+            cont = scanner.next().charAt(0);
+        }
+    }
+
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        char accType;
-
-        AccountType acctType;
-
         System.out.print("What type of account would you like to create? Cheque(C)/Savings(S): ");
         accType = scanner.next().charAt(0);
 
@@ -27,77 +96,12 @@ public class BankApp {
             throw new AccountNotFoundException("Invalid Account");
 
 
-        AccountService accountService;
-
         switch (acctType) {
             case Cheque:
-                char cont;
-                double cash;
-
-                cont = 'Y';
-                accountService = new ChequeAccountService();
-                Account newAccC = accountService.createAccount(1);
-                System.out.println(newAccC.getAccountType() + " Account Created Successfully" + newAccC + "\n");
-
-                while (cont == 'Y') {
-
-
-                    System.out.print("Which Action would you like to perform in your " + newAccC.getAccountType() +
-                            " Account? Deposit(D)/Withdrawal(W)/Check Balance(C): ");
-                    char opt = scanner.next().charAt(0);
-
-                    if (opt == 'D') {
-                        System.out.print("Enter the amout you would like to Deposit: R ");
-                        Scanner depAmount = new Scanner(System.in);
-                        cash = depAmount.nextDouble();
-                        System.out.print("Your Current Balance = R " + accountService.deposit(cash));
-
-                    } else if (opt == 'W') {
-                        System.out.print("Enter the amount to Withdraw: R");
-                        Scanner withAmt = new Scanner(System.in);
-                        cash = withAmt.nextDouble();
-                        System.out.print("Withdraw Successfully\nYour Current Balance is R " + accountService.withdraw(cash));
-
-                    } else if (opt == 'C') {
-                        System.out.print("Your Current Balance is = R " + accountService.getBalance());
-                    }
-                    System.out.print("\nWould you like to perform another accountService?(Y/N): ");
-                    cont = scanner.next().charAt(0);
-                }
+                createCheck();
                 break;
             case Savings:
-                accountService = new SavingsAccountService();
-                Account saving = accountService.createAccount(1);
-                System.out.println(saving.getAccountType() + " Account Created Successfully" + saving+ "\n");
-                double depositAmount;
-
-                cont = 'Y';
-
-                while (cont == 'Y') {
-                    System.out.print("Which Action would you like to perform in your " + saving.getAccountType() +
-                            " Account? Deposit(D)/Withdrawal(W)/Check Balance(C): ");
-                    char optS = scanner.next().charAt(0);
-                    if (optS == 'D') {
-                        Scanner depAmount = new Scanner(System.in);
-
-                        System.out.print("Enter the amout you would like to Deposit: R");
-                        depositAmount = depAmount.nextDouble();
-
-                        System.out.print("Your Current Balance = R " + accountService.deposit(depositAmount));
-                    } else if (optS == 'W') {
-                        Scanner withAmt = new Scanner(System.in);
-
-                        System.out.print("Enter the amount to Withdraw: R");
-                        depositAmount = withAmt.nextDouble();
-
-                        accountService.withdraw(depositAmount);
-                        System.out.print("Withdraw Successfully\nYour Current Balance is R " + accountService.getBalance());
-                    } else if (optS == 'C') {
-                        System.out.print("Your Current Balance is = R " + accountService.getBalance());
-                    }
-                    System.out.print("\nWould you like to perform another accountService?(Y/N): ");
-                    cont = scanner.next().charAt(0);
-                }
+                createSavings();
                 break;
             default:
                 System.out.print("Account Type doesn't exist");
