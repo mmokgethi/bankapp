c35c9dfe6646355555018badf1a63e59e78a02b5
Modisaotsile Mokgethi <modisamokgethi@gmail.com>
2019-03-07 14:42:57 +0200

    Transaction Log

--- src/main/java/com/bbd/modisa/service/ChequeAccountService.java
+++ src/main/java/com/bbd/modisa/service/ChequeAccountService.java
@@ -1,19 +1,13 @@
 package com.bbd.modisa.service;
 
-import com.bbd.modisa.data.TransactionLog;
 import com.bbd.modisa.exception.InsufficientFundsException;
 import com.bbd.modisa.model.Account;
 import com.bbd.modisa.model.CheckAccount;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class ChequeAccountService implements AccountService {
 
     private static Double checkTransFee = 3.5;
     private double myBal;
-    //private List<String> trans = new ArrayList<String>();
-    TransactionLog tl = new TransactionLog();
 
     public ChequeAccountService(){}
 
@@ -23,7 +17,6 @@ public class ChequeAccountService implements AccountService {
 
     public double deposit(double depositAmount) {
         myBal = ((getBalance() + depositAmount) - checkTransFee);
-        //trans.add("Deposited: + " + depositAmount);
         return myBal;
     }
 
@@ -33,19 +26,9 @@ public class ChequeAccountService implements AccountService {
         }
          else {
             myBal = (getBalance() - withdrawalAmount) - checkTransFee;
-            //trans.add("Withdrawn: R" + withdrawalAmount);
         }
         return myBal;
     }
-
-    /*public void transactions()
-    {
-        for (int i = 0; i < trans.size(); i++)
-        {
-            String myLog = trans.get(i);
-            System.out.println(myLog);
-        }
-    }*/
 
     public double getBalance() {
         return myBal;
